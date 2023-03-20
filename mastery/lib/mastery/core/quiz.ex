387332207ce48727f92caa6141ee867825cfd43e
jeryldev <jeryldev@gmail.com>
2023-03-20 13:36:54 +0800

    compose a quiz from functions

--- mastery/lib/mastery/core/quiz.ex
+++ mastery/lib/mastery/core/quiz.ex
@@ -1,4 +1,6 @@
 defmodule Mastery.Core.Quiz do
+  alias Mastery.Core.{Template, Question, Response}
+
   defstruct title: nil,
             mastery: 3,
             templates: %{},
@@ -7,4 +9,25 @@ defmodule Mastery.Core.Quiz do
             last_response: nil,
             record: %{},
             mastered: []
+
+  def new(fields) do
+    struct!(__MODULE__, fields)
+  end
+
+  def add_template(quiz, fields) do
+    template = Template.new(fields)
+
+    templates =
+      update_in(
+        quiz.templates,
+        [template.category],
+        &add_to_list_or_nil(&1, template)
+      )
+
+    %{quiz | templates: templates}
+  end
+
+  def add_to_list_or_nil(nil, template), do: [template]
+
+  def add_to_list_or_nil(templates, template), do: [template | templates]
 end
