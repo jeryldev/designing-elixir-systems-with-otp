161a22b7a22b0e520dea6a1e5d9bd5905d871e07
jeryldev <jeryldev@gmail.com>
2023-05-01 20:53:33 +0800

    improve the ExUnit infrastructure

---
+++ mastery/test/support/quiz_builders.exs
@@ -0,0 +1,10 @@
+defmodule QuizBuilders do
+  defmacro __using__(_options) do
+    quote do
+      alias Mastery.Core.{Template, Response, Quiz}
+      import QuizBuilders, only: :functions
+    end
+  end
+
+  alias Mastery.Core.{Template, Response, Quiz}
+end
