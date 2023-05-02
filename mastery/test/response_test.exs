c0ada5fb464a8d9bae8fe16c8dbe6c5eeac7d59e
jeryldev <jeryldev@gmail.com>
2023-05-02 09:51:13 +0800

    compose within tests

--- mastery/test/response_test.exs
+++ mastery/test/response_test.exs
@@ -2,6 +2,20 @@ defmodule ResponseTest do
   use ExUnit.Case
   use QuizBuilders
 
+  describe "a right response and a wrong response" do
+    setup [:right, :wrong]
+
+    test "building responses checks answers", %{right: right, wrong: wrong} do
+      assert right.correct
+      refute wrong.correct
+    end
+
+    test "a timestamp is added at a build time", %{right: response} do
+      assert %DateTime{} = response.timestamp
+      assert response.timestamp < DateTime.utc_now()
+    end
+  end
+
   defp quiz() do
     fields = template_fields(generators: %{left: [1], right: [2]})
 
@@ -20,19 +34,5 @@ defmodule ResponseTest do
 
   defp wrong(context) do
     {:ok, Map.put(context, :wrong, response("2"))}
-  end
-
-  describe "a right response and a wrong response" do
-    setup [:right, :wrong]
-
-    test "building responses checks answers", %{right: right, wrong: wrong} do
-      assert right.correct
-      refute wrong.correct
-    end
-
-    test "a timestamp is added at a build time", %{right: response} do
-      assert %DateTime{} = response.timestamp
-      assert response.timestamp < DateTime.utc_now()
-    end
   end
 end
